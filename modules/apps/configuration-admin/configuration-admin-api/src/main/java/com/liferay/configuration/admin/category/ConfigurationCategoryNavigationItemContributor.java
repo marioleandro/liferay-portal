4bb3648ca4e12f1244ccde2b1273cd1ad202a376
Mario Leandro <mario.leandro@liferay.com>
2026-09-15 09:47:35 -0300

    LPD-102716 Add a contribution point for configuration category tabs

---
+++ modules/apps/configuration-admin/configuration-admin-api/src/main/java/com/liferay/configuration/admin/category/ConfigurationCategoryNavigationItemContributor.java
@@ -0,0 +1,35 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.configuration.admin.category;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import java.util.Locale;
+
+/**
+ * @author Mario Leandro
+ */
+public interface ConfigurationCategoryNavigationItemContributor {
+
+	public String getCategoryKey();
+
+	public String getKey();
+
+	public String getName(Locale locale);
+
+	public default boolean isVisible() {
+		return true;
+	}
+
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException;
+
+}
